088bd00503df8d5175f3392907cc50ddb2036110
Fernando Salas <luisfernandosalasg@gmail.com>
2021-07-03 05:33:39 -0500

    Add Bubble Sort

--- 12.- Sorting Tecniques/1.- Bubble Sort/main.c
+++ 12.- Sorting Tecniques/1.- Bubble Sort/main.c
@@ -184,12 +184,76 @@ void MergeSort(int A[], int l, int h)
     }
 }
 
+int findMax(int A[], int n)
+{
+    int max = 2147483647;
+    int i;
+    for(i = 0; i<n; i++)
+    {
+        if(A[i]>max)
+        {
+            max = A[i];
+        }
+    }
+    return max;
+}
+
+void CountSort(int A[], int n)
+{
+    int i, j, max, *C;
+    max = findMax(A, n);
+    C = (int*)malloc(sizeof(int)*(max+1));
+    for(i = 0; i<max+1; i++)
+    {
+        C[i] = 0;
+    }
+
+    for(i = 0; i<n; i++)
+    {
+        C[A[i]]++;
+    }
+
+    i = 0; j = 0;
+    while(j < max+1)
+    {
+        if(C[j] > 0)
+        {
+            A[i++] = j;
+            C[j]--;
+        }
+        else
+        {
+            j++;
+        }
+    }
+}
+
+void ShellSort(int A[], int n)
+{
+    int gap, i, j, temp;
+    for(gap = n/2; gap>=1; gap/=2)
+    {
+        for(i = gap; i<n; i++)
+        {
+            temp = A[i];
+            j = i-gap;
+            while(j >= 0 && A[j] > temp)
+            {
+                A[j+gap] = A[j];
+                j = j-gap;
+            }
+            A[j+gap] = temp;
+        }
+    }
+}
 
 
 int main()
 {
     int A[] = {13,7,9,10,6,5,12,4,11,2}, n = 10, i;
-    MergeSort(A, 0, 9);
+    ShellSort(A, n);
+
+
     display(A);
 
     return 0;
